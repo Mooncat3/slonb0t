e3ad7802885f1b6ff99b8a86ff00272951a11775
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-08-04 23:22:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ HOST = "irc.twitch.tv"
 PORT = 6667
 NICK = "SLONB0T"
 PASS = "oauth:1jfryqh0pt9e4uyvhvdl22hk2v9w7a"
+#PASS = "oauth:p4b8xlg6twwgs7izeooy57or9fxlza" #mooncat3
 CHAN = "jesusavgn"
 #CHAN = "mooncat3"
 
@@ -117,50 +118,65 @@ def main():
 
             if respon.find('+do') != -1:
                 buffer = username
-                with open('do.txt', 'r', encoding='utf-8') as c:
-                    listme = list(c)
-                    randomdo = random.choice(listme)
-                    randomdo = re.sub("\n", '', randomdo)
-                #randomdo = respon[respon.index("+do") + len("+do") + 1:len(respon)]
-                do = str.replace(respon, '+do ', '')
-                do = re.sub("\n", '', do)
-                #mess(s, user + randomdo)
-                mess(s, buffer + " садит " + str(do) + " на бутылку YEP")
-                buffer = ''
+                if respon.find('http') != -1 or respon.find('www') != -1 or respon.find('!') != -1 or respon.find('\.') != -1 or respon.find('suicide') != -1:
+                    mess(s, "думал забанить меня? WeirdChamp ")
+                else:
+                    with open('do.txt', 'r', encoding='utf-8') as c:
+                        listme = list(c)
+                        randomdo = random.choice(listme)
+                        randomdo = re.sub("\n", '', randomdo)
+                        #randomdo = respon[respon.index("+do") + len("+do") + 1:len(respon)]
+                        do = str.replace(respon, '+do ', '')
+                        do = re.sub("\n", '', do)
+                        #mess(s, user + randomdo)
+                        mess(s, buffer + " садит " + str(do) + " на бутылку YEP")
+                        buffer = ''
 
 
             if respon.find('+бубу') != -1:
-                bubu = str.replace(respon, '+бубу ', '')
-                bubu = re.sub("\n", '', bubu)
-                mess(s, "Ну " + str(bubu) + " и " + str(bubu) +". Чё бубнить-то? ThumbUp")
+                if respon.find('http') != -1 or respon.find('www') != -1 or respon.find('!') != -1 or respon.find('\.') != -1 or respon.find('suicide') != -1:
+                    mess(s, "думал забанить меня? WeirdChamp ")
+                else:
+                    bubu = str.replace(respon, '+бубу ', '')
+                    bubu = re.sub("\n", '', bubu)
+                    mess(s, "Ну " + str(bubu) + " и " + str(bubu) +". Чё бубнить-то? ThumbUp")
 
 
             if respon.find('+love ') != -1:
                 buffer = username
-                love = str.replace(respon, '+love ', '')
-                love = re.sub("\n", '', love)
-                mess(s, buffer + " любит " + str(love) + " на " + str(procent) + "%!")
-                buffer = ''
+                if respon.find('http') != -1 or respon.find('www') != -1 or respon.find('!') != -1 or respon.find('\.') != -1 or respon.find('suicide') != -1:
+                    mess(s, "думал забанить меня? WeirdChamp ")
+                else:
+                    love = str.replace(respon, '+love ', '')
+                    love = re.sub("\n", '', love)
+                    mess(s, buffer + " любит " + str(love) + " на " + str(procent) + "%!")
+                    buffer = ''
 
 
             if respon.find('+steal ') != -1:
                 buffer = username
-                steal = str.replace(respon, '+steal ', '')
-                steal = re.sub("\n", '', steal)
-                mess(s, buffer + " украл у " + str(steal) + " " + str(ruble) + " руб. BOP")
-                buffer = ''
+                if respon.find('http') != -1 or respon.find('www') != -1 or respon.find('!') != -1 or respon.find('\.') != -1 or respon.find('suicide') != -1:
+                    mess(s, "думал забанить меня? WeirdChamp ")
+                else:
+                    steal = str.replace(respon, '+steal ', '')
+                    steal = re.sub("\n", '', steal)
+                    mess(s, buffer + " украл у " + str(steal) + " " + str(ruble) + " руб. BOP")
+                    buffer = ''
 
 
             if respon.find('+try ') != -1:
                 buffer = username
-                tryy = str.replace(respon, '+try ', '')
-                tryy = re.sub("\n", '', tryy)
-                with open('try.txt', 'r', encoding='utf-8') as m:
-                    listtry = list(m)
-                    tryr = random.choice(listtry)
-                    tryr = re.sub("\n", '', tryr)
-                mess(s, buffer + " попробовал " + str(tryy) + "... " + str(tryr))
-                buffer = ''
+                if respon.find('http') != -1 or respon.find('www') != -1 or respon.find('!') != -1 or respon.find('\.') != -1 or respon.find('suicide') != -1:
+                    mess(s, "думал забанить меня? WeirdChamp ")
+                else:
+                    tryy = str.replace(respon, '+try ', '')
+                    tryy = re.sub("\n", '', tryy)
+                    with open('try.txt', 'r', encoding='utf-8') as m:
+                        listtry = list(m)
+                        tryr = random.choice(listtry)
+                        tryr = re.sub("\n", '', tryr)
+                    mess(s, buffer + " попробовал " + str(tryy) + "... " + str(tryr))
+                    buffer = ''
 
 
             if respon.find('+time') != -1:
@@ -169,6 +185,20 @@ def main():
                 data = datetime.datetime.today().strftime("%d.%m.%Y %H:%M:%S")
                 mess(s, "Чичас " + str(data) + " По МСК Waiting")
 
+            if respon.find('+hug') != -1:
+                buffer = username
+                if respon.find('http') != -1 or respon.find('www') != -1 or respon.find('!') != -1 or respon.find('\.') != -1 or respon.find('suicide') != -1:
+                    mess(s, "думал забанить меня? WeirdChamp ")
+                else:
+                    with open('hug.txt', 'r', encoding='utf-8') as j:
+                        hugg = list(j)
+                        randomhug = random.choice(hugg)
+                        randomhug = re.sub("\n", '', randomhug)
+                        hug = str.replace(respon, '+hug ', '')
+                        hug = re.sub("\n", '', hug)
+                        mess(s, buffer + " " + str(randomhug) + " обнимает " + str(hug) + " hehCat ")
+                
+                
                 
                 
             if respon.find('blushW') != -1:
@@ -198,13 +228,16 @@ def main():
 
             if respon.find('+привет') != -1:
                 buffer = username
-                with open('privet.txt', 'r', encoding='utf-8') as c:
-                    privit = list(c)
-                    randommm = random.choice(privit)
-                    randommm = re.sub("\n", '', randommm)
-                    privet = str.replace(respon, '+привет ', '')
-                    privet = re.sub("\n", '', privet)
-                    mess(s, buffer + " передаёт " + str(randommm) + " привет " + str(privet) + " peepoHey peepoLove")
+                if respon.find('http') != -1 or respon.find('www') != -1 or respon.find('!') != -1 or respon.find('\.') != -1 or respon.find('suicide') != -1:
+                    mess(s, "думал забанить меня? WeirdChamp ")
+                else:
+                    with open('privet.txt', 'r', encoding='utf-8') as c:
+                        privit = list(c)
+                        randommm = random.choice(privit)
+                        randommm = re.sub("\n", '', randommm)
+                        privet = str.replace(respon, '+привет ', '')
+                        privet = re.sub("\n", '', privet)
+                        mess(s, buffer + " передаёт " + str(randommm) + " привет " + str(privet) + " peepoHey peepoLove")
 
 print("Бот " + NICK + " запущен на канале "+str(CHAN)+"!")
 if __name__ == "__main__":
