155cb374f02aa67681aa188b7cb88ac47d012c42
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-08-04 19:19:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,38 +56,53 @@ def main():
 
 
             if respon.find('+help') != -1:
-                mess(s, username + ", Привет, я бот по имени слон catJAM Можешь использовать следующие команды: +test, +me, +do [nickname], +iq, +temp, +love [nickname], +бубу [something], +steal [nickname], +привет [nickname], +try [something], +time, +когда [something] catJAM")
+                buffer = username
+                mess(s, buffer + ", Привет, я бот по имени слон catJAM Можешь использовать следующие команды: +test, +me, +do [nickname], +iq, +temp, +love [nickname], +бубу [something], +steal [nickname], +привет [nickname], +try [something], +time, +когда [something] catJAM")
+                buffer = ''
 
 
             if respon.find('@slonb0t') != -1:
-                mess(s, username + ", Привет, я бот по имени слон catJAM Можешь использовать следующие команды: +test, +me, +do [nickname], +iq, +temp, +love [nickname], +бубу [something], +steal [nickname], +привет [nickname], +try [something], +time, +когда [something] catJAM")
+                buffer = username
+                mess(s, buffer + ", Привет, я бот по имени слон catJAM Можешь использовать следующие команды: +test, +me, +do [nickname], +iq, +temp, +love [nickname], +бубу [something], +steal [nickname], +привет [nickname], +try [something], +time, +когда [something] catJAM")
+                buffer = ''
 
 
             if respon.find('+iq') != -1:
+                buffer = username
                 if iq == 110:
-                    mess(s, username + " ваш IQ = " + str(iq) + "! Вы Хесус?! PogU")
+                    mess(s, buffer + " ваш IQ = " + str(iq) + "! Вы Хесус?! PogU")
+                    buffer = ''
                 if iq == 89:
-                    mess(s, username + " ваш IQ = " + str(iq)+"! Вы Братишкин?! PogU")
+                    mess(s, buffer + " ваш IQ = " + str(iq)+"! Вы Братишкин?! PogU")
+                    buffer = ''
                 else:
                     if iq < 110 and iq > 70:
-                        mess(s, username + " ваш IQ = " + str(iq) + "! Надо же, у стримера больше IQ чем у вас KeK")
+                        mess(s, buffer + " ваш IQ = " + str(iq) + "! Надо же, у стримера больше IQ чем у вас KeK")
+                        buffer = ''
                     if iq > 110 and iq < 135:
-                        mess(s, username + " ваш IQ = " + str(iq) + "! Ого, а вы не глупый человек ThumbUp")
+                        mess(s, buffer + " ваш IQ = " + str(iq) + "! Ого, а вы не глупый человек ThumbUp")
+                        buffer = ''
                     if iq < 70:
-                        mess(s, username + " ваш IQ = " + str(iq) + "! Чел... сходи книгу почитай WeirdChamp")
+                        mess(s, buffer + " ваш IQ = " + str(iq) + "! Чел... сходи книгу почитай WeirdChamp")
+                        buffer = ''
                     if iq >= 135:
-                        mess(s, username + " ваш IQ = " + str(iq) + "! Внимание! В чате гений WAYTOOSMART Clap")
+                        mess(s, buffer + " ваш IQ = " + str(iq) + "! Внимание! В чате гений WAYTOOSMART Clap")
+                        buffer = ''
 
 
             if respon.find('+temp') != -1:
+                buffer = username
                 if temp >= 35.7 and temp <= 37:
-                     mess(s, username + " ваша температура " + str(temp) + " °C! У вас температура в пределах нормы ThumbUp")
+                     mess(s, buffer + " ваша температура " + str(temp) + " °C! У вас температура в пределах нормы ThumbUp")
+                     buffer = ''
                 else:
                     if temp > 37 and temp < 40 or temp < 35.7 and temp >= 32:
-                        mess(s, username + " ваша температура " + str(temp) + " °C! Вы больны? coronaS")
+                        mess(s, buffer + " ваша температура " + str(temp) + " °C! Вы больны? coronaS")
+                        buffer = ''
                     else:
                         if temp > 40 or temp < 32:
-                            mess(s, username + " ваша температура " + str(temp) + " °C! Срочно вызывайте скорую! Durka")
+                            mess(s, buffer + " ваша температура " + str(temp) + " °C! Срочно вызывайте скорую! Durka")
+                            buffer = ''
 
 
             if respon.find('+me') != -1:
@@ -101,6 +116,7 @@ def main():
 
 
             if respon.find('+do') != -1:
+                buffer = username
                 with open('do.txt', 'r', encoding='utf-8') as c:
                     listme = list(c)
                     randomdo = random.choice(listme)
@@ -109,7 +125,8 @@ def main():
                 do = str.replace(respon, '+do ', '')
                 do = re.sub("\n", '', do)
                 #mess(s, user + randomdo)
-                mess(s, username + " садит " + str(do) + " на бутылку YEP")
+                mess(s, buffer + " садит " + str(do) + " на бутылку YEP")
+                buffer = ''
 
 
             if respon.find('+бубу') != -1:
@@ -119,25 +136,31 @@ def main():
 
 
             if respon.find('+love ') != -1:
+                buffer = username
                 love = str.replace(respon, '+love ', '')
                 love = re.sub("\n", '', love)
-                mess(s, username + " любит " + str(love) + " на " + str(procent) + "%!")
+                mess(s, buffer + " любит " + str(love) + " на " + str(procent) + "%!")
+                buffer = ''
 
 
             if respon.find('+steal ') != -1:
+                buffer = username
                 steal = str.replace(respon, '+steal ', '')
                 steal = re.sub("\n", '', steal)
-                mess(s, username + " украл у " + str(steal) + " " + str(ruble) + " руб. BOP")
+                mess(s, buffer + " украл у " + str(steal) + " " + str(ruble) + " руб. BOP")
+                buffer = ''
 
 
             if respon.find('+try ') != -1:
+                buffer = username
                 tryy = str.replace(respon, '+try ', '')
                 tryy = re.sub("\n", '', tryy)
                 with open('try.txt', 'r', encoding='utf-8') as m:
                     listtry = list(m)
                     tryr = random.choice(listtry)
                     tryr = re.sub("\n", '', tryr)
-                mess(s, username + " попробовал " + str(tryy) + "... " + str(tryr))
+                mess(s, buffer + " попробовал " + str(tryy) + "... " + str(tryr))
+                buffer = ''
 
 
             if respon.find('+time') != -1:
@@ -165,21 +188,23 @@ def main():
 
 
             if respon.find('+когда') != -1:
+                buffer = username
                 with open('kogda.txt', 'r', encoding='utf-8') as m:
                     listkogda = list(m)
                     kogda = random.choice(listkogda)
                     kogda = re.sub("\n", '', kogda)
-                mess(s,username + ", " + kogda)
+                mess(s, buffer + ", " + kogda)
 
 
             if respon.find('+привет') != -1:
+                buffer = username
                 with open('privet.txt', 'r', encoding='utf-8') as c:
                     privit = list(c)
                     randommm = random.choice(privit)
                     randommm = re.sub("\n", '', randommm)
                     privet = str.replace(respon, '+привет ', '')
                     privet = re.sub("\n", '', privet)
-                    mess(s, username + " передаёт " + str(randommm) + " привет " + str(privet) + " peepoHey peepoLove")
+                    mess(s, buffer + " передаёт " + str(randommm) + " привет " + str(privet) + " peepoHey peepoLove")
 
 print("Бот " + NICK + " запущен на канале "+str(CHAN)+"!")
 if __name__ == "__main__":
