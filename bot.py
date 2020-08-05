7dd279fdc2d594bf6c2c1873a071dbab35ab8790
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-08-05 03:13:29 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,234 @@
+from irc.bot import SingleServerIRCBot
+import re
+import socket
+import random
+import datetime
+import time
+
+HOST = 'irc.twitch.tv'
+PORT = 6667
+USERNAME = 'SLONB0T'
+PASSWORD = 'oauth:1jfryqh0pt9e4uyvhvdl22hk2v9w7a'
+CHANNEL = '#jesusavgn'
+
+
+class VBot(SingleServerIRCBot):
+
+
+    def nick(self, newnick):
+        """Send a NICK command."""
+        self.send_items('NICK', newnick)
+        print(newnick)
+
+
+
+
+    def __init__(self, host, port, nickname, password, channel):
+        SingleServerIRCBot.__init__(self, [(host, port, password)], nickname, nickname)
+        self.channel = channel
+
+    def on_welcome(self, connection, event):
+        connection.join(self.channel)
+        print("Бот " + USERNAME + " запущен на канале " + str(CHANNEL) + "!")
+
+
+    def on_pubmsg(self, connection, event):
+        message = event.arguments[0]
+        nick = event.source.nick
+        message = event.arguments[0]
+
+        username = nick
+
+
+        print(nick  + ': ' + message)
+
+
+        iq = random.randrange(50, 200, 1)
+        tempp = random.uniform(25, 45)
+        temp = round(tempp, 1)
+        procent = random.randrange(0, 100, 1)
+        ruble = random.randrange(0, 2000, 1)
+
+
+
+        if message.find('+test') != -1:
+            self.connection.privmsg(event.target, 'сидя дома cmonDance грущу чего боюсь cmonDance иду в магаз cmonDance покупаю себе снюс cmonDance и обретаю силы cmonDance я получаю кайф cmonDance снюс моя жизнь снюс is my life cmonDance')
+
+        if message.find('+help') != -1:
+            buffer = username
+            self.connection.privmsg(event.target, buffer + ", Привет, я бот по имени слон catJAM Можешь использовать следующие команды: +test, +me, +do [nickname], +iq, +temp, +love [nickname], +бубу [something], +steal [nickname], +привет [nickname], +try [something], +time, +когда [something], +hug [nickname] catJAM")
+            buffer = ''
+
+        if message.find('@slonb0t') != -1:
+            buffer = username
+            self.connection.privmsg(event.target, buffer + ", Привет, я бот по имени слон catJAM Можешь использовать следующие команды: +test, +me, +do [nickname], +iq, +temp, +love [nickname], +бубу [something], +steal [nickname], +привет [nickname], +try [something], +time, +когда [something], +hug [nickname] catJAM")
+            buffer = ''
+
+        if message.find('+iq') != -1:
+            buffer = username
+            if iq == 110:
+                self.connection.privmsg(event.target, buffer + " ваш IQ = " + str(iq) + "! Вы Хесус?! PogU")
+                buffer = ''
+            if iq == 89:
+                self.connection.privmsg(event.target, buffer + " ваш IQ = " + str(iq) + "! Вы Братишкин?! PogU")
+                buffer = ''
+            else:
+                if iq < 110 and iq > 70:
+                    self.connection.privmsg(event.target, buffer + " ваш IQ = " + str(iq) + "! Надо же, у стримера больше IQ чем у вас KeK")
+                    buffer = ''
+                if iq > 110 and iq < 135:
+                    self.connection.privmsg(event.target, buffer + " ваш IQ = " + str(iq) + "! Ого, а вы не глупый человек ThumbUp")
+                    buffer = ''
+                if iq < 70:
+                    self.connection.privmsg(event.target, buffer + " ваш IQ = " + str(iq) + "! Чел... сходи книгу почитай WeirdChamp")
+                    buffer = ''
+                if iq >= 135:
+                    self.connection.privmsg(event.target, buffer + " ваш IQ = " + str(iq) + "! Внимание! В чате гений WAYTOOSMART Clap")
+                    buffer = ''
+
+        if message.find('+temp') != -1:
+            buffer = username
+            if temp >= 35.7 and temp <= 37:
+                self.connection.privmsg(event.target, buffer + " ваша температура " + str(temp) + " °C! У вас температура в пределах нормы ThumbUp")
+                buffer = ''
+            else:
+                if temp > 37 and temp < 40 or temp < 35.7 and temp >= 32:
+                    self.connection.privmsg(event.target, buffer + " ваша температура " + str(temp) + " °C! Вы больны? coronaS")
+                    buffer = ''
+                else:
+                    if temp > 40 or temp < 32:
+                        self.connection.privmsg(event.target, buffer + " ваша температура " + str(temp) + " °C! Срочно вызывайте скорую! Durka")
+                        buffer = ''
+
+        if message.find('+me') != -1:
+            buffer = username
+            with open('me.txt', 'r', encoding='utf-8') as b:
+                listme = list(b)
+                randomm = random.choice(listme)
+                randomm = re.sub("\n", '', randomm)
+            self.connection.privmsg(event.target, buffer + ' ' + randomm)
+            buffer = ''
+
+        if message.find('+do') != -1:
+            buffer = username
+            if message.find('!') != -1 or message.find('suicide') != -1:
+                self.connection.privmsg(event.target, buffer + ", думал забанить меня? WeirdChamp ")
+            else:
+                with open('do.txt', 'r', encoding='utf-8') as c:
+                    listme = list(c)
+                    randomdo = random.choice(listme)
+                    randomdo = re.sub("\n", '', randomdo)
+                    # randomdo = message[message.index("+do") + len("+do") + 1:len(message)]
+                    do = str.replace(message, '+do ', '')
+                    do = re.sub("\n", '', do)
+                    # self.connection.privmsg(user + randomdo)
+                    self.connection.privmsg(event.target, buffer + " садит " + str(do) + " на бутылку YEP")
+                    buffer = ''
+
+        if message.find('+бубу') != -1:
+            if message.find('!') != -1 or message.find('suicide') != -1:
+                self.connection.privmsg(event.target, buffer + ", думал забанить меня? WeirdChamp ")
+            else:
+                bubu = str.replace(message, '+бубу ', '')
+                bubu = re.sub("\n", '', bubu)
+                self.connection.privmsg("Ну " + str(bubu) + " и " + str(bubu) + ". Чё бубнить-то? ThumbUp")
+
+        if message.find('+love ') != -1:
+            buffer = username
+            if message.find('!') != -1 or message.find('suicide') != -1:
+                self.connection.privmsg(event.target, buffer + ", думал забанить меня? WeirdChamp ")
+            else:
+                love = str.replace(message, '+love ', '')
+                love = re.sub("\n", '', love)
+                self.connection.privmsg(event.target, buffer + " любит " + str(love) + " на " + str(procent) + "%!")
+                buffer = ''
+
+        if message.find('+steal ') != -1:
+            buffer = username
+            if message.find('!') != -1 or message.find('suicide') != -1:
+                self.connection.privmsg(event.target, buffer + ", думал забанить меня? WeirdChamp ")
+            else:
+                steal = str.replace(message, '+steal ', '')
+                steal = re.sub("\n", '', steal)
+                self.connection.privmsg(event.target, buffer + " украл у " + str(steal) + " " + str(ruble) + " руб. BOP")
+                buffer = ''
+
+        if message.find('+try ') != -1:
+            buffer = username
+            if message.find('!') != -1 or message.find('suicide') != -1:
+                self.connection.privmsg(event.target, buffer + ", думал забанить меня? WeirdChamp ")
+            else:
+                tryy = str.replace(message, '+try ', '')
+                tryy = re.sub("\n", '', tryy)
+                with open('try.txt', 'r', encoding='utf-8') as m:
+                    listtry = list(m)
+                    tryr = random.choice(listtry)
+                    tryr = re.sub("\n", '', tryr)
+                self.connection.privmsg(event.target, buffer + " попробовал " + str(tryy) + "... " + str(tryr))
+                buffer = ''
+
+        if message.find('+time') != -1:
+            self.connection.privmsg(event.target, "Таксс... PepoG ")
+            time.sleep(2)
+            data = datetime.datetime.today().strftime("%d.%m.%Y %H:%M:%S")
+            self.connection.privmsg(event.target, "Чичас " + str(data) + " По МСК Waiting")
+
+        if message.find('+hug') != -1:
+            buffer = username
+            if message.find('!') != -1 or message.find('suicide') != -1:
+                self.connection.privmsg(event.target, buffer + ", думал забанить меня? WeirdChamp ")
+            else:
+                with open('hug.txt', 'r', encoding='utf-8') as j:
+                    hugg = list(j)
+                    randomhug = random.choice(hugg)
+                    randomhug = re.sub("\n", '', randomhug)
+                    hug = str.replace(message, '+hug ', '')
+                    hug = re.sub("\n", '', hug)
+                    self.connection.privmsg(event.target, buffer + " " + str(randomhug) + " обнимает " + str(hug) + " VoHiYo")
+
+        if message.find('blushW') != -1:
+            self.connection.privmsg(event.target, "blushW")
+
+        if message.find('peepoLeave') != -1:
+            self.connection.privmsg(event.target, "peepoLeave")
+
+        if message.find('catJAM') != -1:
+            self.connection.privmsg(event.target, "catJAM")
+
+        if message.find('ThumbUp') != -1:
+            self.connection.privmsg(event.target, "ThumbUp")
+
+        if message.find('+когда') != -1:
+            buffer = username
+            with open('kogda.txt', 'r', encoding='utf-8') as m:
+                listkogda = list(m)
+                kogda = random.choice(listkogda)
+                kogda = re.sub("\n", '', kogda)
+            self.connection.privmsg(event.target, buffer + ", " + kogda)
+
+        if message.find('+привет') != -1:
+            buffer = username
+            if message.find('!') != -1 or message.find('suicide') != -1:
+                self.connection.privmsg(event.target, buffer + ", думал забанить меня? WeirdChamp ")
+            else:
+                with open('privet.txt', 'r', encoding='utf-8') as c:
+                    privit = list(c)
+                    randommm = random.choice(privit)
+                    randommm = re.sub("\n", '', randommm)
+                    privet = str.replace(message, '+привет ', '')
+                    privet = re.sub("\n", '', privet)
+                    self.connection.privmsg(event.target, buffer + " передаёт " + str(randommm) + " привет " + str(privet) + " peepoHey peepoLove")
+
+
+
+    @staticmethod
+    def _parse_nickname_from_twitch_user_id(user_id):
+        return user_id.split('!', 1)[0]
+
+def main():
+    my_bot = VBot(HOST, PORT, USERNAME, PASSWORD, CHANNEL)
+    my_bot.start()
+
+
+if __name__ == '__main__':
+    main()
