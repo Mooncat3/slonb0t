5cfab9b558d9d3664571f3952884f6d556bc244b
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-08-09 19:37:36 +0300

    Update TESTbot.py

--- TESTbot.py
+++ TESTbot.py
@@ -1,15 +1,12 @@
 from twitchio.ext import commands
-from datetime import datetime, timedelta
-import forApiCalls
 import json
 import requests
 import re
-import random
 
 class Bot(commands.Bot):
 
     def __init__(self):
-        super().__init__(irc_token='oauth:{}'.format(forApiCalls.OAUTH), client_id=forApiCalls.CLIENT_ID, nick='SLONB0T', prefix='@', initial_channels=['danantur'])
+        super().__init__(irc_token='oauth:2ed7e435kk3dm1tpgo73gnu7xcjczy', client_id=forApiCalls.CLIENT_ID, nick='SLONB0T', prefix='@', initial_channels=['danantur'])
 
     async def event_ready(self):
         print(f'Ready | {self.nick}')
@@ -25,24 +22,27 @@ class Bot(commands.Bot):
         url = "https://aiproject.ru/api/"
         query = {"ask": message, "userid": nickname, "key": ""}
         jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
-        print(jsonquery)
         response = requests.post(url=url, data={"query": jsonquery})
-
         content = response.content.decode('utf8').replace("'", '"')
-
-        print(content)
-
         data = json.loads(content)
+        await s(nickname + ", " + data['aiml'])
+        
+    @commands.command(name='slonb0t, ')
+    async def privet1(self, ctx):
+        message = ctx.message.content
+        nickname = ctx.author.name
+        s = ctx.send
+        url = "https://aiproject.ru/api/"
+        query = {"ask": message, "userid": nickname, "key": ""}
+        jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
+        response = requests.post(url=url, data={"query": jsonquery})
+        content = response.content.decode('utf8').replace("'", '"')
+        data = json.loads(content)
+        await s(nickname + ", " + data['aiml'])   
+   
 
-        await s(data['aiml'])
-
-
-
-
-
-def main():
-    bot = Bot()
-    bot.run()
+bot = Bot()
+bot.run()
 
 if __name__ == '__main__':
     main()
