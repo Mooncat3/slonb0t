44ec71afab6974e67d79ee6aa560a511face04e6
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-08-10 19:32:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ class Bot(commands.Bot):
         super().__init__(irc_token='oauth:2ed7e435kk3dm1tpgo73gnu7xcjczy', client_id='9qmki7jzmtz6qnjj4z35yucfn29xb9', nick='SLONB0T', prefix='@', initial_channels=['danantur'])
 
     async def event_ready(self):
-        print(f'Ready | {self.nick}')
+        print(f'Ready ChatBot | {self.nick}')
 
     @commands.command(name='SLONB0T')
     async def privet(self, ctx):
@@ -21,12 +21,10 @@ class Bot(commands.Bot):
         mess = re.sub("\n", '', mess)
         url = "https://aiproject.ru/api/"
         query = {"ask": mess, "userid": nickname, "key": ""}
-        print(query)
         jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
         response = requests.post(url=url, data={"query": jsonquery})
         content = response.content.decode('utf8').replace("'", '"')
         data = json.loads(content)
-        print(data)
         await s(nickname + ", " + forApiCalls.parse_response_query(data))
 
     @commands.command(name='slonb0t,')
@@ -34,16 +32,14 @@ class Bot(commands.Bot):
         message = ctx.message.content
         nickname = ctx.author.name
         s = ctx.send
-        mess = str.replace(message, '@slonb0t ', '')
+        mess = str.replace(message, '@slonb0t, ', '')
         mess = re.sub("\n", '', mess)
         url = "https://aiproject.ru/api/"
         query = {"ask": mess, "userid": nickname, "key": ""}
-        print(query)
         jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
         response = requests.post(url=url, data={"query": jsonquery})
         content = response.content.decode('utf8').replace("'", '"')
         data = json.loads(content)
-        print(data)
         await s(nickname + ", " + forApiCalls.parse_response_query(data))
 
 bot = Bot()
