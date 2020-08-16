8772c45ecc04e7293a6bb502a3e768ca2a1d1a83
danantur <69195128+danantur@users.noreply.github.com>
2020-08-16 17:57:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ BROADCASTER_ID = "34711476"
 OAUTH = "2ed7e435kk3dm1tpgo73gnu7xcjczy"
 CLIENT_ID = "gp762nuuoqcoxypju8c569th9wz7q5"
 BOT = "SLONB0T"
-CHANNELS = ['danantur']
-CHAN = "danantur"
+CHANNELS = ['jesusavgn']
+CHAN = "jesusavgn"
