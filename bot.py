396ba980e815b9dc7186f8d6f355cfbda4d1d76d
danantur <69195128+danantur@users.noreply.github.com>
2020-08-16 14:23:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,16 +37,13 @@ class Bot(commands.Bot, ABC):
         message = ctx.message.content
         message = str.replace(message, '!archive', '')
         print(message)
-        try:
-            id=int(message[1:2])
-            if len(message[2:len(message)]) > 1:
-                tag = str.replace(message, f' {id} ', '')
-            else:
-                tag = ''
-            if AdditionalMethods.check_on_bans(message, ctx.author):
-                AdditionalMethods.add_to_buffer("c", AdditionalMethods.get_archive_stream_stat(id, nickname, tag).format(nickname, id), ctx.author)
-        except:
-            AdditionalMethods.add_to_buffer("c", f'{nickname} !archive [0-9]', ctx.author)
+        id=int(message[1:2])
+        if len(message[2:len(message)]) > 1:
+            tag = str.replace(message, f' {id} ', '')
+        else:
+            tag = ''
+        if AdditionalMethods.check_on_bans(message, ctx.author):
+            AdditionalMethods.add_to_buffer("c", AdditionalMethods.get_archive_stream_stat(id, nickname, tag).format(nickname, id), ctx.author)
             
     @commands.command(name='рецепт')
     async def recept(self, ctx):
