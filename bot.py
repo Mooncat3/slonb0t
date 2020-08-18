b822a1e1eb13f0fc6c79fec813fc9ffca9a35d3b
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-08-18 03:35:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,15 +34,15 @@ class Bot(commands.Bot, ABC):
             
     @commands.command(name='заебало')
     async def zaebalo(self, ctx):
-    randpage = random.randrange(1, 1689, 1)
-    r = requests.get("https://zaebalo.ru/?page=" + str(randpage))
-    soup = BeautifulSoup(r.content, 'html.parser')
-    d = soup.find_all('div', align='left')
-    p = str(random.choice(d)).replace("</div>", "").replace("<br/>", "").replace("</p>", "").replace("<p>", "").replace('<div align="left">', '').replace("<br>", "").replace("</br>", "")
-    while 'пидор' in p or 'негр' in p or len(p) > 500:
+        randpage = random.randrange(1, 1689, 1)
+        r = requests.get("https://zaebalo.ru/?page=" + str(randpage))
+        soup = BeautifulSoup(r.content, 'html.parser')
         d = soup.find_all('div', align='left')
         p = str(random.choice(d)).replace("</div>", "").replace("<br/>", "").replace("</p>", "").replace("<p>", "").replace('<div align="left">', '').replace("<br>", "").replace("</br>", "")
-    await ctx.send(p)
+        while 'пидор' in p or 'негр' in p or len(p) > 500:
+            d = soup.find_all('div', align='left')
+            p = str(random.choice(d)).replace("</div>", "").replace("<br/>", "").replace("</p>", "").replace("<p>", "").replace('<div align="left">', '').replace("<br>", "").replace("</br>", "")
+        await ctx.send(p)
 
     @commands.command(name='archive')
     async def streamh(self, ctx):
