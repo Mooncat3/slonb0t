f6014787fb34c025f0ac72d68f74c8eb32137493
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-08-23 21:03:29 +0300

    Update LOGS.py

--- LOGS.py
+++ LOGS.py
@@ -9,6 +9,12 @@ with open(file='data/TRASHMASSIVE.txt', mode='r', encoding='utf-8') as q:
 g = Github("f0011283768114fac26230cd23b3208ed10d0a54")
 
 repo = g.search_repositories("slonb0t")[0]
+
+contents = repo.get_contents("data/TRASHMASSIVE.txt")
+repo.delete_file(contents.path, "Automated Remove from Bot", contents.sha)
+
+time.sleep(5)
+
 repo.create_file("data/TRASHMASSIVE.txt", "Automated Upload from Bot", strer)
 
 time.sleep(300)
