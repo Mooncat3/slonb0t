58a43e2b898fe2879dff93eba50c52ffce982fa9
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-08-23 04:18:49 +0300

    Add files via upload

---
+++ LOGS.py
@@ -0,0 +1,25 @@
+import AdditionalMethods
+from bs4 import BeautifulSoup
+import requests
+import time
+
+
+
+with open(file='data/TRASHMASSIVE.txt', mode='r', encoding='utf-8') as q:
+    strer = q.read()
+paste = AdditionalMethods.createPaste(strer, "loges", "php", "1", "10M")
+urlPaste = AdditionalMethods.sendPaste(paste)
+
+
+teg = urlPaste[21:]
+
+print(teg)
+
+r = requests.get('https://pastebin.com/raw/' + str(teg))
+soup = BeautifulSoup(r.content, 'html.parser')
+d = soup.get_text()
+
+with open(file='data/TRASHMASSIVE.txt', mode='w', encoding='utf-8') as e:
+    e.write(str(d))
+    
+time.sleep(1800)
