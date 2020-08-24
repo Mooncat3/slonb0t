27ea66515a5cb87a78dbd36cf848cd29e79057ce
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-08-24 15:47:17 +0300

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -44,6 +44,21 @@ class ChatBot(commands.Bot, ABC):
         content = response.content.decode('utf8').replace("'", '"')
         data = json.loads(content)
         await AdditionalMethods.add_to_buffer("e", nickname + ", " + AdditionalMethods.parse_response_query(data), ctx.author)
+        
+        
+    @commands.command(name="slonb0t")
+    async def privet1(self, ctx):
+        message = ctx.message.content
+        nickname = ctx.author.name
+        mess = str.replace(message, '@slonb0t ', '')
+        mess = re.sub("\n", '', mess)
+        url = "https://aiproject.ru/api/"
+        query = {"ask": mess, "userid": nickname, "key": ""}
+        jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
+        response = requests.post(url=url, data={"query": jsonquery})
+        content = response.content.decode('utf8').replace("'", '"')
+        data = json.loads(content)
+        await AdditionalMethods.add_to_buffer("e", nickname + ", " + AdditionalMethods.parse_response_query(data), ctx.author)
 
 
 
