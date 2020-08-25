806f5d2504e83d5d3b26d71f075754fcd92f8e6a
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-08-26 01:01:51 +0300

    Update CheckBans.py

--- CheckBans.py
+++ CheckBans.py
@@ -1,8 +1,7 @@
-while True:
-    ss = input()
-    with open('data/osujdau.txt', 'r', encoding='utf_8') as f:
-        l = [line.strip() for line in f]
-        if any(x in ss for x in l):
-            print('ОСУЖДАЮ')
-        else:
-            print('НЕ ОСУЖДАЮ')
+words = input() #вводимое предложение
+with open('data/osujdau.txt', 'r', encoding='utf_8') as f:
+    l = [line.strip() for line in f]
+    if any(x in words for x in l):
+        print('НАЙДЕНО ОСУДИТЕЛЬНОЕ СЛОВО')
+    else:
+        print('НЕ НАЙДЕНО ОСУДИТЕЛЬНЫХ СЛОВ')
