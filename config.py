546885ddaa1aacc097efbd971c93078353c297ca
danantur <69195128+danantur@users.noreply.github.com>
2020-08-27 12:06:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BROADCASTER_ID = "34711476"
-OAUTH = "vj6bru9m36zvqalnst2myuynutukr6"
+OAUTH = "14y5qalllj1i65rg3m9dip1rpq5ugd"
 CLIENT_ID = "gp762nuuoqcoxypju8c569th9wz7q5"
 BOT = "SLONB0T"
 CHANNELS = ['jesusavgn']
