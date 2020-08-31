30fc8fd966824d62b48619690ddc3954dced4855
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-08-31 15:16:33 +0300

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -1,4 +1,4 @@
-from abc import ABC
+# -*- coding: utf8 -*-
 from twitchioc.ext import commands
 import json
 import AdditionalMethods
@@ -7,60 +7,62 @@ import requests
 import config
 
 
-class ChatBot(commands.Bot, ABC):
+bot = commands.Bot(
+    irc_token=f'oauth:{config.OAUTH},
+    client_id=config.CLIENT_ID,
+    nick=config.BOT,
+    prefix='@',
+    initial_channels=config.CHANNELS)
 
-    def __init__(self):
-        super().__init__(irc_token=f'oauth:{config.OAUTH}',
-                         client_id=config.CLIENT_ID, nick=config.BOT, prefix='@', initial_channels=config.CHANNELS)
+@bot.event
+    async def event_ready():
+        print(f'Ready ChatBot | {config.BOT} on {config.CHANNELS}')
 
-    async def event_ready(self):
-        print(f'Ready {str(self.__class__.__name__)} | {self.nick} on {self.initial_channels}')
-        
+@bot.event
     async def event_command_error(ctx, error):
-        return
+        return True
 
-    @commands.command(name='SLONB0T')
-    async def privet(self, ctx):
-        message = ctx.message.content
-        nickname = ctx.author.name
-        mess = str.replace(message, '@SLONB0T ', '')
-        mess = re.sub("\n", '', mess)
-        url = "https://aiproject.ru/api/"
-        query = {"ask": mess, "userid": nickname, "key": ""}
-        jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
-        response = requests.post(url=url, data={"query": jsonquery})
-        content = response.content.decode('utf8').replace("'", '"')
-        data = json.loads(content)
-        await AdditionalMethods.add_to_buffer("e", nickname + ", " + AdditionalMethods.parse_response_query(data), ctx.author, "SLONB0T")
+@bot.command(name='SLONB0T')
+async def chat(ctx):
+    message = ctx.message.content
+    nickname = ctx.author.name
+    mess = str.replace(message, '@SLONB0T ', '')
+    mess = re.sub("\n", '', mess)
+    url = "https://aiproject.ru/api/"
+    query = {"ask": mess, "userid": nickname, "key": ""}
+    jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
+    response = requests.post(url=url, data={"query": jsonquery})
+    content = response.content.decode('utf8').replace("'", '"')
+    data = json.loads(content)
+    await AdditionalMethods.add_to_buffer("e", nickname + ", " + AdditionalMethods.parse_response_query(data), ctx.author, "SLONB0T")
 
-    @commands.command(name="slonb0t,")
-    async def privet1(self, ctx):
-        message = ctx.message.content
-        nickname = ctx.author.name
-        mess = str.replace(message, '@slonb0t, ', '')
-        mess = re.sub("\n", '', mess)
-        url = "https://aiproject.ru/api/"
-        query = {"ask": mess, "userid": nickname, "key": ""}
-        jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
-        response = requests.post(url=url, data={"query": jsonquery})
-        content = response.content.decode('utf8').replace("'", '"')
-        data = json.loads(content)
-        await AdditionalMethods.add_to_buffer("e", nickname + ", " + AdditionalMethods.parse_response_query(data), ctx.author, "slonb0t,")
-        
-    @commands.command(name="slonb0t")
-    async def privet1(self, ctx):
-        message = ctx.message.content
-        nickname = ctx.author.name
-        mess = str.replace(message, '@slonb0t ', '')
-        mess = re.sub("\n", '', mess)
-        url = "https://aiproject.ru/api/"
-        query = {"ask": mess, "userid": nickname, "key": ""}
-        jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
-        response = requests.post(url=url, data={"query": jsonquery})
-        content = response.content.decode('utf8').replace("'", '"')
-        data = json.loads(content)
-        await AdditionalMethods.add_to_buffer("e", nickname + ", " + AdditionalMethods.parse_response_query(data), ctx.author, "slonb0t")    
- 
+@bot.command(name="slonb0t,")
+async def chat1(ctx):
+    message = ctx.message.content
+    nickname = ctx.author.name
+    mess = str.replace(message, '@slonb0t, ', '')
+    mess = re.sub("\n", '', mess)
+    url = "https://aiproject.ru/api/"
+    query = {"ask": mess, "userid": nickname, "key": ""}
+    jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
+    response = requests.post(url=url, data={"query": jsonquery})
+    content = response.content.decode('utf8').replace("'", '"')
+    data = json.loads(content)
+    await AdditionalMethods.add_to_buffer("e", nickname + ", " + AdditionalMethods.parse_response_query(data), ctx.author, "slonb0t,")
 
-bot = ChatBot()
+@bot.command(name="slonb0t")
+async def chat2(ctx):
+    message = ctx.message.content
+    nickname = ctx.author.name
+    mess = str.replace(message, '@slonb0t ', '')
+    mess = re.sub("\n", '', mess)
+    url = "https://aiproject.ru/api/"
+    query = {"ask": mess, "userid": nickname, "key": ""}
+    jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
+    response = requests.post(url=url, data={"query": jsonquery})
+    content = response.content.decode('utf8').replace("'", '"')
+    data = json.loads(content)
+    await AdditionalMethods.add_to_buffer("e", nickname + ", " + AdditionalMethods.parse_response_query(data), ctx.author, "slonb0t")    
+
+    
 bot.run()
