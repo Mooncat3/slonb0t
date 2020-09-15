78c36817262707ac79007e7147f4123d99cd478f
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-09-15 09:08:34 +0300

    Delete CheckBans.py

--- CheckBans.py
+++
@@ -1,7 +0,0 @@
-words = input() #вводимое предложение
-with open('data/osujdau.txt', 'r', encoding='utf_8') as f:
-    l = [line.strip() for line in f]
-    if any(x in words for x in l):
-        print('НАЙДЕНО ОСУДИТЕЛЬНОЕ СЛОВО')
-    else:
-        print('НЕ НАЙДЕНО ОСУДИТЕЛЬНЫХ СЛОВ')
