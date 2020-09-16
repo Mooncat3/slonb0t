df3fc3019254b075fa84ce7b8b8b978d0e518abb
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-09-16 19:55:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,15 @@
+
+
+CHAN = "jesusavgn"
+
+
 BROADCASTER_ID = "34711476"
 OAUTH = "14y5qalllj1i65rg3m9dip1rpq5ugd"
 CLIENT_ID = "gp762nuuoqcoxypju8c569th9wz7q5"
 BOT = "SLONB0T"
-CHANNELS = ['jesusavgn']
-CHAN = "jesusavgn"
+CHANNELS = [f'{CHAN}']
 buferchanged = False
 istopcliprunning = False
 helpUrl = "https://pastebin.com/raw/hZ4GGw4z"
 abreviationsUrl = "https://pastebin.com/raw/h546CMvM"
 api_url = "https://slon-api.herokuapp.com"
-
-"""
-OAUTH = "vj6bru9m36zvqalnst2myuynutukr6"
-CLIENT_ID = "gp762nuuoqcoxypju8c569th9wz7q5"
-BOT = "SLONB0T"
-"""
