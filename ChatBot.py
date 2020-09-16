ee0e1e25d70163e2dc43aee84e04a73b06fca697
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-09-16 19:05:16 +0300

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -22,65 +22,28 @@ async def event_ready():
 async def event_command_error(ctx, error):
     pass
 
-@bot.command(name='SLONB0T')
-async def chat(ctx):
-    message = ctx.message.content
-    nickname = ctx.author.name
-    mess = str.replace(message, '@SLONB0T ', '')
-    mess = re.sub("\n", '', mess)
-    url = "https://aiproject.ru/api/"
-    query = {"ask": mess, "userid": nickname, "key": ""}
-    jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
-    try:
-        with requests.sessions.Session() as session:
-            response = session.request(method="post", url=url, data={"query": jsonquery}, timeout=3)
-        content = response.content.decode('utf8').replace("'", '"')
-        data = json.loads(content)
-        AdditionalMethods.add_to_buffer("e", nickname + ", " + AdditionalMethods.parse_response_query(data),
-                                        ctx.author, "SLONB0T")
-    except:
-        AdditionalMethods.add_to_buffer("e", nickname + ", На данный момент чатбот не доступен roflanPominy",
-                                        ctx.author, "SLONB0T")
-
-@bot.command(name="slonb0t,")
-async def chat1(ctx):
-    message = ctx.message.content
-    nickname = ctx.author.name
-    mess = str.replace(message, '@slonb0t, ', '')
-    mess = re.sub("\n", '', mess)
-    url = "https://aiproject.ru/api/"
-    query = {"ask": mess, "userid": nickname, "key": ""}
-    jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
-    try:
-        with requests.sessions.Session() as session:
-            response = session.request(method="post", url=url, data={"query": jsonquery}, timeout=3)
-        content = response.content.decode('utf8').replace("'", '"')
-        data = json.loads(content)
-        AdditionalMethods.add_to_buffer("e", nickname + ", " + AdditionalMethods.parse_response_query(data),
-                                        ctx.author, "SLONB0T")
-    except:
-        AdditionalMethods.add_to_buffer("e", nickname + ", На данный момент чатбот не доступен roflanPominy",
-                                        ctx.author, "SLONB0T")
-
-@bot.command(name="slonb0t")
-async def chat2(ctx):
-    message = ctx.message.content
-    nickname = ctx.author.name
-    mess = str.replace(message, '@slonb0t ', '')
-    mess = re.sub("\n", '', mess)
-    url = "https://aiproject.ru/api/"
-    query = {"ask": mess, "userid": nickname, "key": ""}
-    jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
-    try:
-        with requests.sessions.Session() as session:
-            response = session.request(method="post", url=url, data={"query": jsonquery}, timeout=3)
-        content = response.content.decode('utf8').replace("'", '"')
-        data = json.loads(content)
-        AdditionalMethods.add_to_buffer("e", nickname + ", " + AdditionalMethods.parse_response_query(data),
-                                        ctx.author, "SLONB0T")
-    except:
-        AdditionalMethods.add_to_buffer("e", nickname + ", На данный момент чатбот не доступен roflanPominy",
-                                        ctx.author, "SLONB0T") 
-
+@bot.event
+async def event_message(ctx):
     
+    if ctx.content.lower().find('slonb0t') != -1:
+        message = ctx.content
+        nickname = ctx.author.name
+        mess = message.lower().replace("slonb0t","").replace("@","")
+        mess = re.sub("\n", '', mess)
+        url = "https://aiproject.ru/api/"
+        query = {"ask": mess, "userid": nickname, "key": ""}
+        jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
+        try:
+            with requests.sessions.Session() as session:
+                response = session.request(method="post", url=url, data={"query": jsonquery}, timeout=3)
+            content = response.content.decode('utf8').replace("'", '"')
+            data = json.loads(content)
+            AdditionalMethods.add_to_buffer("e", nickname + ", " + AdditionalMethods.parse_response_query(data),
+                                            ctx.author, "SLONB0T")
+        except:
+            AdditionalMethods.add_to_buffer("e", nickname + ", На данный момент чатбот не доступен roflanPominy",
+                                            ctx.author, "SLONB0T")
+    await bot.handle_commands(ctx)
+    
+
 bot.run()
