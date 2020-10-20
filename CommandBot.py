0fca5d07dbe0ad0fc16fc2aef8ce0e28cc5ec382
danantur <69195128+danantur@users.noreply.github.com>
2020-10-20 19:28:15 +0300

    Update CommandBot.py

--- CommandBot.py
+++ CommandBot.py
@@ -1011,7 +1011,7 @@ class CommandsBot(commands.Bot, ABC):
         result = AdditionalMethods.parse_standartfile_message(nickname, "{nickname} {filestr} обнимает {messagestr} "
                                                                         "VoHiYo",
                                                               message, "!обнять", "hug")
-        if not AdditionalMethods.check_on_toomuchbool(result):
+        if not len(message) > 50:
             AdditionalMethods.add_to_buffer("e", result, ctx.author, "обнять")
         else:
             AdditionalMethods.add_to_buffer("e", f"{nickname}, пишите меньше символов WeirdChamp", ctx.author, "обнять")
