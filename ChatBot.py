0f3954421a6ac2840e4fc4e0ce4216c0d01a4807
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-11-07 20:57:56 +0300

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -35,13 +35,13 @@ async def chat(ctx):
                 response = session.request(method="post", url=url, data={"query": jsonquery}, timeout=3)
             content = response.content.decode('utf8').replace("'", '"')
             data = json.loads(content)
-            AdditionalMethods.add_to_buffer("e", nickname + ", " + AdditionalMethods.parse_response_query(data),
+            AdditionalMethods.add_to_buffer("e", ctx.author.display_name + ", " + AdditionalMethods.parse_response_query(data),
                                             ctx.author, "SLONB0T")
         except:
-            AdditionalMethods.add_to_buffer("e", nickname + ", На данный момент чатбот не доступен roflanPominy",
+            AdditionalMethods.add_to_buffer("e", ctx.author.display_name + ", На данный момент чатбот не доступен roflanPominy",
                                             ctx.author, "SLONB0T")
     else:
-        AdditionalMethods.add_to_buffer("e", nickname + ", спик рашн WeirdChamp",
+        AdditionalMethods.add_to_buffer("e", ctx.author.display_name + ", спик рашн WeirdChamp",
                                             ctx.author, "SLONB0T")
 
     
