7a04dde5d79f9114296254375bb9063aac4a8587
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-11-16 10:27:56 +0300

    Update CommandBot.py

--- CommandBot.py
+++ CommandBot.py
@@ -889,8 +889,8 @@ class CommandsBot(commands.Bot, ABC):
     async def puk(self, ctx):
         global namess
         nickname = ctx.author.display_name
-        namess = list(set(namess))
-        AdditionalMethods.add_to_buffer("e", nickname + ' пукнул на ' + random.choice(namess) + ' (puke) ThumbUp', ctx.author, "пук")
+        namesss = list(set(namess))
+        AdditionalMethods.add_to_buffer("e", nickname + ' пукнул на ' + random.choice(namesss) + ' (puke) ThumbUp', ctx.author, "пук")
 
     @commands.command(name='do')
     async def do(self, ctx):
