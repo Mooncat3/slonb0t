4f6d76c2b4b99435bbc300dfe33f62aeb2504a0a
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-11-17 22:36:57 +0300

    Add files via upload

--- stream/streambudet.py
+++ stream/streambudet.py
@@ -3,16 +3,20 @@ from twitchio.ext import commands
 
 with open('1.txt', 'r', encoding='utf_8') as t:
     global one
-    one = list(set(t.read().split('\n')))
+    one = t.read().split('\n')
     
 with open('2.txt', 'r', encoding='utf_8') as j:
     global two
-    two = list(set(j.read().split('\n')))
+    two = j.read().split('\n')
     
 with open('3.txt', 'r', encoding='utf_8') as k:
     global three
-    three = list(set(k.read().split('\n')))
+    three = k.read().split('\n')
+
 print(one, two, three)
+
+four = ['хде', 'где', 'гдe', 'хдe']
+
 bot = commands.Bot(
         irc_token='oauth:14y5qalllj1i65rg3m9dip1rpq5ugd',
         nick='SLONB0T',
@@ -21,13 +25,10 @@ bot = commands.Bot(
 
 @bot.event
 async def event_ready():
-    global i
-    i=0
     print("Бот запущен!")
 
 @bot.event
 async def event_message(ctx):
-    global i
     mess = ctx.content.lower()
     nick = ctx.author.name
     if nick == 'slonb0t' or nick == 'moobot':
@@ -36,13 +37,14 @@ async def event_message(ctx):
         if mess.find('!') != -1:
             pass
         else:
-            if any(mess.find(x) != -1 for x in one):
-                if any(mess.find(x) != -1 for x in two):
-                    if any(mess.find(x) != -1 for x in three):
-                        #await ctx.channel.send(ctx.author.display_name+", стрима сегодня не будет. Вся инфа в телеге - https://t.me/jesusavgntwitch PunOko")
-                        await ctx.channel.send(ctx.author.display_name+", стрима сегодня не будет FeelsBadMan")
-                        i += 1
-                        print(i, end='\r')
+            if any(mess.find(x) != -1 for x in four):
+                if any(mess.find(x) != -1 for x in one):
+                    await ctx.channel.send(ctx.author.display_name+", стрима сегодня не будет FeelsBadMan")
+            else:
+                if any(mess.find(x) != -1 for x in one):
+                    if any(mess.find(x) != -1 for x in two):
+                        if any(mess.find(x) != -1 for x in three):
+                            await ctx.channel.send(ctx.author.display_name+", стрима сегодня не будет FeelsBadMan")
     await bot.handle_commands(ctx)
-            
+
 bot.run()
