f0852acf37c96f44daa72158e499b554b52b557c
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-11-17 22:06:12 +0300

    Add files via upload

---
+++ stream/streambudet.py
@@ -0,0 +1,48 @@
+from twitchio.ext import commands
+
+
+with open('1.txt', 'r', encoding='utf_8') as t:
+    global one
+    one = t.read().split('\n')
+    
+with open('2.txt', 'r', encoding='utf_8') as j:
+    global two
+    two = j.read().split('\n')
+    
+with open('3.txt', 'r', encoding='utf_8') as k:
+    global three
+    three = k.read().split('\n')
+print(one, two, three)
+bot = commands.Bot(
+        irc_token='oauth:14y5qalllj1i65rg3m9dip1rpq5ugd',
+        nick='SLONB0T',
+        prefix='&',
+        initial_channels=['jesusavgn'])
+
+@bot.event
+async def event_ready():
+    global i
+    i=0
+    print("Бот запущен!")
+
+@bot.event
+async def event_message(ctx):
+    global i
+    mess = ctx.content.lower()
+    nick = ctx.author.name
+    if nick == 'slonb0t' or nick == 'moobot':
+        pass
+    else:
+        if mess.find('!') != -1:
+            pass
+        else:
+            if any(mess.find(x) != -1 for x in one):
+                if any(mess.find(x) != -1 for x in two):
+                    if any(mess.find(x) != -1 for x in three):
+                        #await ctx.channel.send(ctx.author.name+", стрима сегодня не будет. Вся инфа в телеге - https://t.me/jesusavgntwitch PunOko")
+                        await ctx.channel.send(ctx.author.name+", стрима сегодня не будет FeelsBadMan")
+                        i += 1
+                        print(i, end='\r')
+    await bot.handle_commands(ctx)
+            
+bot.run()
