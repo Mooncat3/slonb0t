69d903f8b8994ee7ba147b1030631620ccbfae30
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-11-23 11:00:04 +0300

    Add files via upload

--- stream/streambudet.py
+++ stream/streambudet.py
@@ -13,9 +13,7 @@ with open('3.txt', 'r', encoding='utf_8') as k:
     global three
     three = k.read().split('\n')
 
-print(one, two, three)
-
-four = ['хде', 'где', 'гдe', 'хдe']
+four = ['хде', 'где', 'гди', 'хди', 'когда']
 
 bot = commands.Bot(
         irc_token='oauth:14y5qalllj1i65rg3m9dip1rpq5ugd',
@@ -31,20 +29,28 @@ async def event_ready():
 async def event_message(ctx):
     mess = ctx.content.lower()
     nick = ctx.author.name
+    
+    word = ctx.author.display_name+", стримов в ближающую неделю не будет. Вся информация в телеграм-канале - https://t.me/jesusavgntwitch FeelsBadMan"
+    
     if nick == 'slonb0t' or nick == 'moobot':
         pass
     else:
-        if mess.find('!') != -1:
+        if mess.find('!') != -1 or len(mess) > 80:
             pass
         else:
             if any(mess.find(x) != -1 for x in four):
+                print(mess, 'из списка', '1')
                 if any(mess.find(x) != -1 for x in one):
-                    await ctx.channel.send(ctx.author.display_name+", стрима сегодня не будет FeelsBadMan")
+                    print(mess, 'из списка', 'Отправлено')
+                    await ctx.channel.send(word)
             else:
                 if any(mess.find(x) != -1 for x in one):
+                    print(mess, '1')
                     if any(mess.find(x) != -1 for x in two):
+                        print(mess, '2')
                         if any(mess.find(x) != -1 for x in three):
-                            await ctx.channel.send(ctx.author.display_name+", стрима сегодня не будет FeelsBadMan")
+                            print(mess, 'Отправлено')
+                            await ctx.channel.send(word)
     await bot.handle_commands(ctx)
 
 bot.run()
