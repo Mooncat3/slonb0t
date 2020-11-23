7c67e852961b2fa44f7ae8a0780dfd309114665a
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-11-23 12:43:37 +0300

    Add files via upload

--- stream/streambudet.py
+++ stream/streambudet.py
@@ -1,35 +1,31 @@
 from twitchio.ext import commands
-from abc import ABC
 import asyncio
 
 
-class CommandsBot(commands.Bot, ABC):
+class stream(commands.Bot):
     def __init__(self):
         super().__init__(irc_token='oauth:14y5qalllj1i65rg3m9dip1rpq5ugd',
                          client_id="gp762nuuoqcoxypju8c569th9wz7q5", nick="SLONB0T", prefix='!',
                          initial_channels=['jesusavgn'])
-                         
+
     async def event_ready(self):
-        with open('1.txt', encoding='utf-8') as t:
-            self.one = t.read().split('\n')
-        with open('2.txt', encoding='utf-8') as j:
-            self.two = j.read().split('\n')
-        with open('3.txt', encoding='utf-8') as k:
-            self.three = k.read().split('\n')
-        with open('4.txt', encoding='utf-8') as nn:
-            self.four = nn.read().split('\n')
+        self.one = open('1.txt', encoding='utf-8').read().split('\n')
+        self.two = open('2.txt', encoding='utf-8').read().split('\n')
+        self.three = open('3.txt', encoding='utf-8').read().split('\n')
+        self.four = open('4.txt', encoding='utf-8').read().split('\n')
         print('Бот запущен!')
-        
+
     async def event_command_error(self, ctx, error):
         pass
 
     async def event_message(self, ctx):
-        mess = ctx.content.lower()
         mess_orig = ctx.content
+        mess = mess_orig.lower()
         nick = ctx.author.name
+        nick_dis = ctx.author.display_name
         send = ctx.channel.send
         
-        word = ctx.author.display_name+", стримов в ближающую неделю не будет. Вся информация в телеграм-канале - https://t.me/jesusavgntwitch FeelsBadMan"
+        word = f"{nick_dis}, стримов в ближающую неделю не будет. Вся информация в телеграм-канале - https://t.me/jesusavgntwitch FeelsBadMan"
 
         if nick == 'slonb0t' or nick == 'moobot':
             pass
@@ -38,19 +34,19 @@ class CommandsBot(commands.Bot, ABC):
                 pass
             elif len(mess) < 80:
                 if any(mess.find(x) != -1 for x in self.four):
-                    print(mess_orig, '| из списка |', '1')
+                    print(mess_orig, '| список | 1')
                     if any(mess.find(x) != -1 for x in self.one):
-                        print(mess_orig, '| из списка |', 'Отправлено')
+                        print(mess_orig, '| список | 2 |', 'Отправлено!')
                         await send(word)
                 else:
                     if any(mess.find(x) != -1 for x in self.one):
-                        print(mess_orig, '1')
+                        print(mess_orig, '| 1')
                         if any(mess.find(x) != -1 for x in self.two):
-                            print(mess_orig, '2')
+                            print(mess_orig, '| 2')
                             if any(mess.find(x) != -1 for x in self.three):
-                                print(mess_orig, 'Отправлено')
+                                print(mess_orig, '| 3 | Отправлено!')
                                 await send(word)
         await bot.handle_commands(ctx)
 
-bot = CommandsBot()
+bot = stream()
 bot.run()
