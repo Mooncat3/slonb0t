0cfb669968b97619dd3eb8023c1678b9b56c18aa
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-11-28 14:31:42 +0300

    Update CommandBot.py

--- CommandBot.py
+++ CommandBot.py
@@ -45,7 +45,7 @@ class CommandsBot(commands.Bot, ABC):
         self.japtest = {'kanji': '', 'tr': '', 'example': '', 'tr_example': '', 'active': False}
         with open('data/blacklist.txt', encoding='utf-8') as f:
             self.blacklist = [x for x in f.read().split('\n') if len(x) > 1]
-        print(self.blacklist)
+        print('Blacklist:',self.blacklist)
         
     '''
     async def event_command_error(self, ctx, error):
