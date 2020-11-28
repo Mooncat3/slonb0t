430a6b01d20debbb004833d2ca990fa2e2ad121d
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-11-28 23:47:22 +0300

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -1,52 +1,47 @@
 from twitchioc.ext import commands
 import json
+from abc import ABC
 import AdditionalMethods
 import re
 import requests
 import config
 
-bot = commands.Bot(
-    irc_token=f'oauth:{config.OAUTH}',
-    client_id=config.CLIENT_ID,
-    nick=config.BOT,
-    prefix='@',
-    initial_channels=config.CHANNELS)
 
+class ChatBot(commands.Bot, ABC):
+    def __init__(self):
+        super().__init__(irc_token=f'oauth:{config.OAUTH}', client_id=config.CLIENT_ID, nick=config.BOT, prefix='@', initial_channels=config.CHANNELS)
 
-@bot.event
-async def event_ready():
-    print(f'Ready ChatBot | {config.BOT} on {config.CHANNELS[0]}')
+    async def event_ready(self):
+        print(f'Ready ChatBot | {config.BOT} on {config.CHANNELS[0]}')
 
+    async def event_command_error(self, ctx, error):
+        pass
 
-@bot.event
-async def event_command_error(ctx, error):
-    pass
+    async def event_message(self, message):
+        nickname = message.author.name
+        with open('data/blacklist.txt', encoding='utf-8') as f:
+            blacklist = [x for x in f.read().split('\n') if len(x) > 1]
+        if nickname in blacklist and not AdditionalMethods.vip(message.author.is_mod, nickname):
+            return
+        
+    @commands.command(name='SLONB0T', aliases=["slonb0t,", "slonb0t"])
+    async def chat(self, ctx):
+        message = ctx.message.clean_content
+        nickname = ctx.author.name
+        if not re.search(r'[a-zA-Z]', message) or AdditionalMethods.vip(ctx.author.is_mod, ctx.author.name):
+            url = "https://aiproject.ru/api/"
+            query = {"ask": message, "userid": nickname, "key": ""}
+            jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
+            try:
+                with requests.sessions.Session() as session:
+                    response = session.request(method="post", url=url, data={"query": jsonquery}, timeout=3)
+                content = response.content.decode('utf8').replace("'", '"')
+                data = json.loads(content)
+                AdditionalMethods.add_to_buffer("e", ctx.author.display_name + ", " + AdditionalMethods.parse_response_query(data), ctx.author, "SLONB0T")
+            except:
+                AdditionalMethods.add_to_buffer("e", ctx.author.display_name + ", На данный момент чатбот не доступен roflanPominy", ctx.author, "SLONB0T")
+        else:
+            AdditionalMethods.add_to_buffer("s", ctx.author.display_name + ", бот может общаться с вами только на русском языке", ctx.author, "SLONB0T")
 
-
-@bot.command(name='SLONB0T', aliases=["slonb0t,", "slonb0t"])
-async def chat(ctx):
-    message = str(ctx.message.clean_content)
-    nickname = ctx.author.name
-    if not re.search(r'[a-zA-Z]', message) or AdditionalMethods.vip(ctx.author.is_mod, ctx.author.name):
-        url = "https://aiproject.ru/api/"
-        query = {"ask": message, "userid": nickname, "key": ""}
-        jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
-        try:
-            with requests.sessions.Session() as session:
-                response = session.request(method="post", url=url, data={"query": jsonquery}, timeout=3)
-            content = response.content.decode('utf8').replace("'", '"')
-            data = json.loads(content)
-            AdditionalMethods.add_to_buffer("e",
-                                            ctx.author.display_name + ", " + AdditionalMethods.parse_response_query(
-                                                data),
-                                            ctx.author, "SLONB0T")
-        except:
-            AdditionalMethods.add_to_buffer("e", ctx.author.display_name + ", На данный момент чатбот не доступен "
-                                                                           "roflanPominy",
-                                            ctx.author, "SLONB0T")
-    else:
-        AdditionalMethods.add_to_buffer("s", ctx.author.display_name + ", бот может общаться с вами только на русском "
-                                                                       "языке", ctx.author, "SLONB0T")
-
-
+bot = ChatBot()
 bot.run()
