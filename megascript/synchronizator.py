8ca04131d39355f7f4262e5d0ee010100ada1611
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-11-29 19:33:27 +0300

    Update synchronizator.py

--- megascript/synchronizator.py
+++ megascript/synchronizator.py
@@ -11,26 +11,17 @@ from cook import PREFS, COOKIES
 import time
 import json
 
-
-
-#Екарны бабай, закидывай cook.py и chromedriver.exe со скриптом Pepechill
-
 data = requests.get(f"http://tmi.twitch.tv/group/user/jesusavgn/chatters").json()
 
 chatters = []
 for k in data['chatters'].keys():
         chatters += data['chatters'][k]
-
 end_count = len(chatters)
-
 timestart = time.time()
 
 for user in chatters:
-
     should_pass = False
-
     answer = json.loads(requests.get(f"https://sl0n.herokuapp.com/stats/jesusavgn?nickname={user}", headers={"Authorization": "y5IArL6S&%%G(69G"}).content.decode("utf-8"))
-
     if answer['type'] == 'success':
         if 'count_state' in answer['answer'].keys():
             if answer['answer']['count_state'] > 1:
@@ -49,16 +40,16 @@ for user in chatters:
         print(f"//------------- Данный пользователь ЛОХ! {user} {listnames[user]} -------------------//")
         print(requests.post("https://sl0n.herokuapp.com/stats/jesusavgn",
                             data={'type': 'clear', 'nickname': user},
-                            headers={"Authorization": "y5IArL6S&%%G(69G"}).content)
+                            headers={"Authorization": "y5IArL6S&%%G(69G"}).text)
         print(
             requests.post("https://sl0n.herokuapp.com/stats/jesusavgn",
                           data={'type': 'add', 'nickname': user, 'count': listnames[user]},
-                          headers={"Authorization": "y5IArL6S&%%G(69G"}).content)
+                          headers={"Authorization": "y5IArL6S&%%G(69G"}).text)
     else:
         print(f"//------------- Идёт сканирование сообщений пользователя {user} -------------------//")
         print(requests.post("https://sl0n.herokuapp.com/stats/jesusavgn",
                             data={'type': 'clear', 'nickname': user},
-                            headers={"Authorization": "y5IArL6S&%%G(69G"}).content)
+                            headers={"Authorization": "y5IArL6S&%%G(69G"}).text)
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -69,10 +60,10 @@ for user in chatters:
         driver.add_cookie(COOKIES[0])
         driver.get(url)
         try:
-            WebDriverWait(driver, timeout=6).until(ec.visibility_of_element_located((By.CLASS_NAME, "tw-c-text-link")))
+            WebDriverWait(driver, timeout=10).until(ec.visibility_of_element_located((By.CLASS_NAME, "tw-c-text-link")))
             mess = driver.find_element_by_xpath('//p[starts-with(@class, "tw-c-text-link tw-font-size-5 tw-strong")]').text
             if mess == '999+':
-                WebDriverWait(driver, timeout=3).until(ec.visibility_of_element_located((By.CLASS_NAME, "text-fragment")))
+                WebDriverWait(driver, timeout=10).until(ec.visibility_of_element_located((By.CLASS_NAME, "text-fragment")))
                 driver.find_element_by_xpath('//span[starts-with(@class, "text-fragment")]').click()
                 elem = driver.find_element_by_class_name('simplebar-scrollbar')
                 while elem.is_displayed():
@@ -81,7 +72,7 @@ for user in chatters:
             print(f"Пользователь {user} написал {str(mess)} сообщений!")
             print(requests.post("https://sl0n.herokuapp.com/stats/jesusavgn",
                               data={'type': 'add', 'nickname': user, 'count': mess},
-                              headers={"Authorization": "y5IArL6S&%%G(69G"}).content)
+                              headers={"Authorization": "y5IArL6S&%%G(69G"}).text)
         except TimeoutException:
             print(f"Не удалось узнать кол-во сообщений {user}")
         driver.quit()
