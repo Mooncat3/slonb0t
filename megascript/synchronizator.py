223c9ab2f1b3c3c9441e44c12010b262bf66b530
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-12-04 21:28:56 +0300

    Add files via upload

--- megascript/synchronizator.py
+++ megascript/synchronizator.py
@@ -1,7 +1,7 @@
 import requests
 import datetime
-import time
 import json
+import time
 
 
 Client_ID = 'kimne78kx3ncx6brgo4mv6wki5h1ko'
@@ -9,15 +9,13 @@ OAUTH = 'l4tt0z3a94edvjo3kbs0c3s4qimpsp'
 url = 'https://gql.twitch.tv/gql'
 head = {'Authorization': f'OAuth {OAUTH}', 'Client-ID': Client_ID}
 head_2 = {'Authorization': f'Bearer {OAUTH}', 'Client-ID': Client_ID}
+cursor = ''
+ii = 0
 while True:
-    data = requests.get(f"http://tmi.twitch.tv/group/user/jesusavgn/chatters").json()
-
-    chatters = []
-    for k in data['chatters'].keys():
-            chatters += data['chatters'][k]
-    end_count = len(chatters)
-    timestart = time.time()
-
+    time_start = time.time()
+    data_2 = requests.get(f'https://api.twitch.tv/helix/users/follows?to_id=34711476&first=100&after={cursor}', headers=head_2).json()
+    cursor = data_2['pagination']['cursor']
+    chatters = [user['from_name'] for user in data_2['data']]
     for user in chatters:
         should_pass = False
         answer = requests.get(f"https://sl0n.herokuapp.com/stats/jesusavgn?nickname={user}", headers={"Authorization": "y5IArL6S&%%G(69G"}).json()
@@ -34,22 +32,20 @@ while True:
             listnames[json_r[i]['name']] = json_r[i]['amount']
 
         if should_pass:
-            print(f"//------------- Данный пользователь уже синхронизирован! {user} -------------------//")
+            print(f"//-------- Данный пользователь уже синхронизирован! {user} --------//")
         elif user in listnames.keys():
-            print(f"//------------- Данный пользователь ЛОХ! {user} {listnames[user]} -------------------//")
-            print(requests.post("https://sl0n.herokuapp.com/stats/jesusavgn",
-                                data={'type': 'clear', 'nickname': user},
-                                headers={"Authorization": "y5IArL6S&%%G(69G"}).text)
-            print(
-                requests.post("https://sl0n.herokuapp.com/stats/jesusavgn",
-                              data={'type': 'add', 'nickname': user, 'count': listnames[user]},
-                              headers={"Authorization": "y5IArL6S&%%G(69G"}).text)
+            requests.post("https://sl0n.herokuapp.com/stats/jesusavgn",
+                            data={'type': 'clear', 'nickname': user},
+                            headers={"Authorization": "y5IArL6S&%%G(69G"})
+            requests.post("https://sl0n.herokuapp.com/stats/jesusavgn",
+                            data={'type': 'add', 'nickname': user, 'count': listnames[user]},
+                            headers={"Authorization": "y5IArL6S&%%G(69G"})
         else:
             try:
-                print(f"//------------- Идёт сканирование сообщений пользователя {user} -------------------//")
-                print(requests.post("https://sl0n.herokuapp.com/stats/jesusavgn",
-                                    data={'type': 'clear', 'nickname': user},
-                                    headers={"Authorization": "y5IArL6S&%%G(69G"}).text)
+                print(f"//-------- Сканирование сообщений {user} --------//")
+                requests.post("https://sl0n.herokuapp.com/stats/jesusavgn",
+                                data={'type': 'clear', 'nickname': user},
+                                headers={"Authorization": "y5IArL6S&%%G(69G"})
                 a = []
                 url_2 = 'https://api.twitch.tv/helix/users?login=' + user
                 try:
@@ -70,14 +66,18 @@ while True:
                                 a.append(b['node']['sentAt'])
                             except:
                                 pass
+                        if a[-1] == a[-2]:
+                            break
                         data_loop = a[-1]
-                    except Exception as e:
-                        print(e)
+                    except:
+                        print('ОШИБКА')
+                        break
                 mess_count = len(set(a))
+                requests.post("https://sl0n.herokuapp.com/stats/jesusavgn",
+                                data={'type': 'add', 'nickname': user, 'count': mess_count},
+                                headers={"Authorization": "y5IArL6S&%%G(69G"})
                 print(f'Пользователь {user} написал {mess_count} сообщений')
-                print(requests.post("https://sl0n.herokuapp.com/stats/jesusavgn",
-                                    data={'type': 'add', 'nickname': user, 'count': mess_count},
-                                    headers={"Authorization": "y5IArL6S&%%G(69G"}).text)
-            except:
-                pass
-    print(f"//------------- СКАНИРОВАНИЕ ЗАВЕРШЕНО -------------------//")
+            except Exception as e:
+                print(e)
+    ii += 100
+    print(f"//---------------- ПРОСКАНИРОВАНО: {ii} | {time.time()-time_start} сек. ----------------//")
