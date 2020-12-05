4d9ee58202047289494d1f683fedb5fd76ba927b
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-12-05 03:04:27 +0300

    Add files via upload

--- megascript/synchronizator.py
+++ megascript/synchronizator.py
@@ -9,6 +9,7 @@ OAUTH = 'l4tt0z3a94edvjo3kbs0c3s4qimpsp'
 url = 'https://gql.twitch.tv/gql'
 head = {'Authorization': f'OAuth {OAUTH}', 'Client-ID': Client_ID}
 head_2 = {'Authorization': f'Bearer {OAUTH}', 'Client-ID': Client_ID}
+hash_name = '2c484f8a5ff63f06732707c8ca989083e46b2aa81a03b02e7ac7b9aa9fcba9a2'
 cursor = ''
 ii = 0
 while True:
@@ -53,24 +54,25 @@ while True:
                 except:
                     sender = 0
                 start = datetime.datetime.today()
-                data_loop = start.strftime('%Y-%m-%dT%H:%M:%S.0Z')
+                data_loop = start.strftime('%Y-%m-%dT%H:%M:%SZ|0')
                 while True:
                     try:
-                        json_msg = [{"operationName":"ViewerCardModLogsMessagesBySender","variables":{"senderID":sender,"channelLogin":"jesusavgn","cursor":data_loop+"|12600c60-246e-4cc0-8b7b-0380aa0e5329","includeAutoModCaughtMessages":True},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"2c484f8a5ff63f06732707c8ca989083e46b2aa81a03b02e7ac7b9aa9fcba9a2"}}}]
+                        json_msg = [{"operationName":"ViewerCardModLogsMessagesBySender","variables":{"senderID":sender,"channelLogin":"jesusavgn","cursor":data_loop,"includeAutoModCaughtMessages":True},"extensions":{"persistedQuery":{"version":1,"sha256Hash":hash_name}}}]
                         r = requests.post(url, headers=head, json=json_msg)
                         r_json = r.json()[0]['data']['channel']['modLogs']['messagesBySender']['edges']
                         if len(r_json) < 2:
                             break
                         for b in r_json:
                             try:
-                                a.append(b['node']['sentAt'])
+                                b['node']['sentAt']
+                                a.append(b['cursor'])
                             except:
                                 pass
                         if a[-1] == a[-2]:
                             break
                         data_loop = a[-1]
-                    except:
-                        print('ОШИБКА')
+                    except Exception as e:
+                        print(e)
                         break
                 mess_count = len(set(a))
                 requests.post("https://sl0n.herokuapp.com/stats/jesusavgn",
