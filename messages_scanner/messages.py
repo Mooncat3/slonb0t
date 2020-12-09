4304443d91070de79a4852b57c8e1db2a7569699
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-12-09 09:11:04 +0300

    Update messages.py

--- messages_scanner/messages.py
+++ messages_scanner/messages.py
@@ -11,16 +11,20 @@ head_2 = {'Authorization': f'Bearer {OAUTH}', 'Client-ID': Client_ID}
 hash_name = '2c484f8a5ff63f06732707c8ca989083e46b2aa81a03b02e7ac7b9aa9fcba9a2'
 url_2 = f'https://api.twitch.tv/helix/users?login={nickname}'
 mess_count = 0
-try: sender = requests.get(url_2, headers=head_2).json()['data'][0]['id']
-except IndexError: sender = 0
+try:
+    sender = requests.get(url_2, headers=head_2).json()['data'][0]['id']
+except IndexError:
+    sender = 0
 data_loop = datetime.datetime.today().strftime('%Y-%m-%dT%H:%M:%SZ|0')
 while True:
     try:
         json_msg = [{"operationName":"ViewerCardModLogsMessagesBySender","variables":{"senderID":sender,"channelLogin":"jesusavgn","cursor":data_loop,"includeAutoModCaughtMessages":True},"extensions":{"persistedQuery":{"version":1,"sha256Hash":hash_name}}}]
         r = requests.post(url, headers=head, json=json_msg).json()[0]['data']['channel']['modLogs']['messagesBySender']['edges']
-        if len(r) == 0: break
+        if len(r) == 0:
+            break
         a = [b['cursor'] for b in r if 'sentAt' in b['node']]
         data_loop = a[-1]
         mess_count += len(a)
-    except KeyError: break
-print(f'Пользователь {nickname} написал {mess_count} сообщений | Дата первого сообщения: {data_loop.split("T")[0]}')
+    except:
+        break
+print(f'Пользователь {nickname} написал {mess_count} сообщений | Дата первого сообщения: {data_loop.split(".")[0]}')
