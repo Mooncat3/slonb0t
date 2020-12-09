6e45246f2a2dbc26428ecf16096a21650ad33814
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-12-09 13:23:56 +0300

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -30,20 +30,18 @@ class ChatBot(commands.Bot, ABC):
     async def chat(self, ctx):
         message = ctx.message.clean_content
         nickname = ctx.author.name
-        if not re.search(r'[a-zA-Z]', message) or AdditionalMethods.vip(ctx.author.is_mod, ctx.author.name):
-            url = "https://aiproject.ru/api/"
-            query = {"ask": message, "userid": nickname, "key": ""}
-            jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
-            try:
-                with requests.sessions.Session() as session:
-                    response = session.request(method="post", url=url, data={"query": jsonquery}, timeout=3)
-                content = response.content.decode('utf8').replace("'", '"')
-                data = json.loads(content)
-                AdditionalMethods.add_to_buffer("e", ctx.author.display_name + ", " + AdditionalMethods.parse_response_query(data), ctx.author, "SLONB0T")
-            except:
-                AdditionalMethods.add_to_buffer("e", ctx.author.display_name + ", На данный момент чатбот не доступен roflanPominy", ctx.author, "SLONB0T")
-        else:
-            AdditionalMethods.add_to_buffer("s", ctx.author.display_name + ", бот может общаться с вами только на русском языке", ctx.author, "SLONB0T")
+        message = re.sub(r'[a-zA-Z]', '', message)
+        url = "https://aiproject.ru/api/"
+        query = {"ask": message, "userid": nickname, "key": ""}
+        jsonquery = json.encoder.JSONEncoder.encode(self=json.encoder.JSONEncoder(), o=query)
+        try:
+            with requests.sessions.Session() as session:
+                response = session.request(method="post", url=url, data={"query": jsonquery}, timeout=3)
+            content = response.content.decode('utf8').replace("'", '"')
+            data = json.loads(content)
+            AdditionalMethods.add_to_buffer("e", ctx.author.display_name + ", " + AdditionalMethods.parse_response_query(data), ctx.author, "SLONB0T")
+        except:
+            AdditionalMethods.add_to_buffer("e", ctx.author.display_name + ", На данный момент чатбот не доступен roflanPominy", ctx.author, "SLONB0T")
 
 bot = ChatBot()
 bot.run()
