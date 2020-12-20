ab8fc1cb4e60fdd5422dabe85e1a7eb7b49f8aec
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2020-12-21 01:11:29 +0300

    Update CommandBot.py

--- CommandBot.py
+++ CommandBot.py
@@ -1108,7 +1108,7 @@ class CommandsBot(commands.Bot, ABC):
                                 break
                         if flag:
                             break
-                    res = [x for x in [re.sub(r'[\[].*?[\]]', '', i) for i in res] if len(x) > 1]
+                    res = [x[:25] for x in [re.sub(r'[\[].*?[\]]', '', i) for i in res] if len(x) > 1]
                     res = emote.join(res[:4])
                     with open('data/osujdau2.txt', 'r', encoding='utf-8') as f:
                         osu = [x for x in f.read().split('\n') if len(x) > 1]
