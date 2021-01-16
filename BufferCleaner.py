ecead2df4105e71ae3700073f04f2a3e5fe9feb6
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2021-01-17 01:07:57 +0300

    Update BufferCleaner.py

--- BufferCleaner.py
+++ BufferCleaner.py
@@ -129,7 +129,8 @@ class BufferCleaner(Client, ABC):
                 if rest['vip'] and rest['type'] != "s":
                     await sock.send_privmsg(config.CHAN, mess)
                 elif rest['type'] == "s" or x - excluding >= Settings.get_bufer_max() or time.time() - self.times[str(parse_kd_comand(self.kd, res['command']))] < parse_kd_comand(self.kd, res['command']):
-                    await sock.send_privmsg(config.CHAN, f"/w {rest['nickname']} !{resert['cmd']} ▶ {mess}")
+                    #await sock.send_privmsg(config.CHAN, f"/w {rest['nickname']} !{resert['cmd']} ▶ {mess}")
+                    pass
                 else:
                     pass
             await asyncio.sleep(0.2)
