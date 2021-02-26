08b66d295baaf130152046d76fdb6c4f369834ef
Mooncat3 <59804614+Mooncat3@users.noreply.github.com>
2021-02-26 23:56:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ CHAN = "JesusAVGN"
 
 
 BROADCASTER_ID = "34711476"
-OAUTH = "yj99k68vhsubshagx9ow431sazbnkw"
+OAUTH = "s275j59dh4jsctiazoearlkjl9agjl"
 CLIENT_ID = "gp762nuuoqcoxypju8c569th9wz7q5"
 BOT = "SLONB0T"
 CHANNELS = [f'{CHAN}']
